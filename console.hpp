f805f7cc3f6829abbe51b724a28030ce93e38180
mbcarruthers <mbcarruthers@crimson.ua.edu>
2018-11-13 00:34:06 -0600

    syntax change

--- console.hpp
+++ console.hpp
@@ -11,7 +11,8 @@ public:
     void log( void ) {}
 
     template<typename T , typename... Types>
-    void log(T const& arg ,Types const&...  args) {
+    void log(T const& arg ,Types const&...  args)
+    {
         std::cout << arg << "\n";
         log(args...);
     }
