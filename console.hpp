add9072018829902121b3f70d0c2e8c10b2fa0e0
mbcarruthers <mbcarruthers@crimson.ua.edu>
2018-11-12 23:37:35 -0600

    first commit

---
+++ console.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+class Console {
+private:
+    //none
+
+public:
+    Console() = default;
+
+    void log( void ) {}
+
+    template<typename T , typename... Types>
+    void log(T const& arg ,Types const&...  args) {
+        std::cout << arg << "\n";
+
+        log(args...);
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "console.hpp"
+
+int main( void )
+{
+    Console console{};
+    std::string name{ "your name " };
+    unsigned int int_number = 12;
+    long long long_number = 2'000'000'000'000;
+
+    console.log(name , int_number , long_number);
+    return 0;
+}
