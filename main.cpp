fc8b0b464d7bc844f338b20315409b41c90b1919
mbcarruthers <mbcarruthers@crimson.ua.edu>
2018-11-13 00:42:29 -0600

    small change

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 int main( void )
 {
     Console console{};
+
     std::string name{ "your name " };
     unsigned int int_number = 12;
     long long_number = 2'000'000'000'000;
-    std::cout << long_number << "\n";
+
     console.log(name , int_number , long_number);
     return 0;
 }
